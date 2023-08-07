4ad39e038c814e7eb7e22fb3025bbad0eca0b624
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-08-06 21:51:09 -0700

    style(project setup): added rules and plugins in the eslintrc.json
    Create a set of plugins to use and rules, that made the template for this and other personal
    projects.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useMemo } from 'react';
-import reactLogo from './assets/react.svg';
+import React, { useState } from 'react';
+
 import viteLogo from '/vite.svg';
+
+import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
