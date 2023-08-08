7d1c19b7aa4d244a1cffffcb3b8dac3152742fd1
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-08-07 21:36:33 -0700

    feat(project ui): testing some values of the MUI-Button component, to get it working

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
+        <Button
+          variant="contained"
+          color="warning"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </Button>
         <p>
